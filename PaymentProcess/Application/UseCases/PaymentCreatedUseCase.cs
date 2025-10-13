d1c0285ab104b77eeb9d32d523120c01327615ad
Kako Ferrare <kakoferrare87@gmail.com>
2025-10-13 11:22:06 -0300

    feat: add loggin information

--- PaymentProcess/Application/UseCases/PaymentCreatedUseCase.cs
+++ PaymentProcess/Application/UseCases/PaymentCreatedUseCase.cs
@@ -1,11 +1,19 @@
 ﻿using Domain.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace Application.UseCases
 {
 	public class PaymentCreatedUseCase : IPaymentCreatedUseCase
 	{
+		private readonly ILogger<PaymentCreatedUseCase> _logger;
+
+		public PaymentCreatedUseCase(ILogger<PaymentCreatedUseCase> logger)
+		{
+			_logger = logger;
+		}
 		public void Handle(object message, CancellationToken cancellationToken)
 		{
+			_logger.LogInformation("Start process Payment");
 			Console.WriteLine(message);
 		}
 	}
